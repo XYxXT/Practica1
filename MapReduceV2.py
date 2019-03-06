58df2e8bf528ee2175fbcf5e84d62b508bf3682a
nilde <Nil.Domene@e-campus.uab.cat>
2019-03-06 14:02:31 +0100

    Version sin threads funcionando

--- MapReduceV2.py
+++ MapReduceV2.py
@@ -9,47 +9,62 @@ class MapReduce():
     '''reducing_map es un mapa: {'word1':5, 'word2':6}'''
     global shuffling_map
     global reducing_map
+    global local_lists
     
     def __init__(self):
         self.shuffling_map = {} 
         self.reducing_map = {}
+        self.mapping_map=[]
+        self.local_lists=[]
         
-    def readFile(self):
+    def readFile(self,name):
         '''leer todos los ficheros  y return todos los textos en un string'''
-        return 0
+        self.local_lists=[ln.translate(None, '\n.,').lower() for ln in open(name, 'r')]
+        self.splitting()
+       
+        
     
-    def splitting(self, items):
+    def splitting(self):
         ''' separar el string y crear multiple thread que llamara a mapping'''
-        return 0
+        for line in self.local_lists:
+            self.mapping(line)
+        self.shuffling()
+        
     
     
     def mapping(self, text): 
         ''' items es una cadena de texto '''
         '''cada map es una lista: [['word1',1], ['word2',1], ['word1',1], ['word3',1]...]'''
-        mapping_map = []
-        
-        items = text.split()
-        
-        for item in items:
-            mapping_map.append([item, 1])
 
-        self.shuffling(mapping_map)
         
-    def shuffling(self, mapping_map):
-        for item in mapping_map:
+        items = text.split(" ")
+        for item in items: 
+            self.mapping_map.append([item, 1])
+        
+        
+    def shuffling(self):
+
+        for item in self.mapping_map:
+
             if item[0] not in self.shuffling_map:
                 self.shuffling_map[item[0]] = [item]
             else:
                 self.shuffling_map[item[0]].append(item)  
 
 
+
     def reduce(self):
-        for itemKey, itemValue in sorted(self.shuffling_map.items()):
+        
+        for itemKey, itemValue in self.shuffling_map.iteritems():
             value = 0
             for v in itemValue:
                 value += v[1]
             self.reducing_map[itemKey] = value
                    
-    def removeChars(self,text, chars):
-        for character in chars:
-            text = text.replace(character, "")
+
+mr = MapReduce()
+mr.readFile("./texts/testInput.txt")
+mr.reduce()
+
+for k,v in mr.reducing_map.iteritems():
+    print k +":"+ str(v)
