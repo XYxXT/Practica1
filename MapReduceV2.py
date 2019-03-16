b40ffa3bb6a167d8c4669e85f85c8de460d1f6a3
nilde <Nil.Domene@e-campus.uab.cat>
2019-03-16 17:22:47 +0100

    Threads implementado

--- MapReduceV2.py
+++ MapReduceV2.py
@@ -3,6 +3,8 @@ Created on 3 mar. 2019
 
 @author: XYxXT
 '''
+import threading
+
 class MapReduce():
     '''shuffling_map es un mapa: {'word1':[['word1',1],['word1',1],['word1',1],['word1',1]],
     'word2':[['word2',1],['word2',1],['word2',1],['word2',1]]} '''
@@ -11,24 +13,33 @@ class MapReduce():
     global reducing_map
     global local_lists
     
-    def __init__(self):
+    def __init__(self,numTreads):
         self.shuffling_map = {} 
         self.reducing_map = {}
         self.mapping_map=[]
         self.local_lists=[]
+        self.numTreads=numTreads
         
     def readFile(self,name):
         '''leer todos los ficheros  y return todos los textos en un string'''
-        self.local_lists=[ln.translate(None, '\n.,').lower() for ln in open(name, 'r')]
+        self.local_lists=[ln.translate(None, '\n.,:;').lower() for ln in open(name, 'r')]
         self.splitting()
-       
+        self.reduce()
         
     
     def splitting(self):
         ''' separar el string y crear multiple thread que llamara a mapping'''
-        for line in self.local_lists:
-            self.mapping(line)
-        self.shuffling()
+        
+        #Threads para cada una de las lineas
+        for line in xrange(0,len(self.local_lists)):
+            thread = threading.Thread(target = self.mapping, args = [line])
+            thread.run()
+
+        
+        #threads con indices (thread 0: 1-20,thread 1: 21-40 ...)
+        for i in xrange(0,len(self.mapping_map),len(self.mapping_map)/self.numTreads):
+            thread = threading.Thread(target = self.shuffling, args = (i,i+(len(self.mapping_map)/self.numTreads)))
+            thread.run()
         
     
     
@@ -37,14 +48,14 @@ class MapReduce():
         '''cada map es una lista: [['word1',1], ['word2',1], ['word1',1], ['word3',1]...]'''
 
         
-        items = text.split(" ")
+        items = self.local_lists[text].split(" ")
         for item in items: 
             self.mapping_map.append([item, 1])
         
         
-    def shuffling(self):
+    def shuffling(self,initialIndex,finalIndex):
 
-        for item in self.mapping_map:
+        for item in self.mapping_map[initialIndex:finalIndex]:
 
             if item[0] not in self.shuffling_map:
                 self.shuffling_map[item[0]] = [item]
@@ -62,9 +73,8 @@ class MapReduce():
             self.reducing_map[itemKey] = value
                    
 
-mr = MapReduce()
-mr.readFile("./texts/testInput.txt")
-mr.reduce()
+mr = MapReduce(4)
+mr.readFile("./texts/bible.txt")
 
 for k,v in mr.reducing_map.iteritems():
     print k +":"+ str(v)
